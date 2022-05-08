9f120506058d6f8f9c4861bd4ba124dc09dea469
HunterKF <95839328+HunterKF@users.noreply.github.com>
2022-05-09 08:44:10 +0900

    Updates: - Made CustomItem.kt expandable.
    To-Do:
    - Add functionality to the expanded cards.
    - Make grammar/word clickable

--- app/src/main/java/com/example/koreancompose/CustomItem.kt
+++ app/src/main/java/com/example/koreancompose/CustomItem.kt
@@ -1,66 +1,125 @@
 package com.example.koreancompose
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.koreancompose.model.PracticeCard
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CustomItem(practiceCard: PracticeCard) {
-    Row(
+    var expandedState by remember { mutableStateOf(false) }
+    val rotateState by animateFloatAsState(targetValue = if (expandedState) 180f else 0f)
+
+
+    Card(
         modifier = Modifier
-            .background(Color.White)
             .shadow(2.dp, RoundedCornerShape(16.dp), true)
-            .fillMaxWidth()
-            .padding(24.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+        onClick = {
+            expandedState = !expandedState
+        }
     ) {
         Column(
             modifier = Modifier
-                .weight(1f),
-            verticalArrangement = Arrangement.SpaceBetween
+                .padding(16.dp)
+                .fillMaxWidth()
         ) {
-            Text(
-                text = "${practiceCard.word}",
-                color = Color.DarkGray,
-                fontWeight = FontWeight.Light
-            )
-            Spacer(modifier = Modifier
-                .height(10.dp)
-            )
-            Text(
-                text = "${practiceCard.grammar}",
-                color = Color.DarkGray,
-                fontWeight = FontWeight.Light
-            )
-        }
-        Divider(
-            modifier = Modifier
-                .fillMaxHeight()
-                .width(1.dp)
-        )
-        Text(
-            modifier = Modifier
-                .weight(3f),
-            text = "${practiceCard.inputedSentence}",
-            color = Color.Black,
-            fontWeight = FontWeight.Medium,
-            fontSize = 16.sp,
-            lineHeight = 24.sp,
-            letterSpacing = 0.15.sp
+            Row {
+                Column(
+                    modifier = Modifier
+                        .weight(1f),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "${practiceCard.word}",
+                        color = Color.DarkGray,
+                        fontWeight = FontWeight.Light
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .height(10.dp)
+                    )
+                    Text(
+                        text = "${practiceCard.grammar}",
+                        color = Color.DarkGray,
+                        fontWeight = FontWeight.Light
+                    )
+                }
+                Divider(
+                    modifier = Modifier
+                        .width(1.dp)
+                )
+                Text(
+                    modifier = Modifier
+                        .weight(4f),
+                    text = "${practiceCard.inputedSentence}",
+                    color = Color.Black,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 16.sp,
+                    lineHeight = 24.sp,
+                    letterSpacing = 0.15.sp
 
-        )
-        Spacer(modifier = Modifier.height(10.dp))
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                IconButton(
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium)
+                        .weight(1f)
+                        .rotate(rotateState),
+                    onClick = {
+                        expandedState = !expandedState
+                    }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Drop"
+                    )
+                }
+            }
+
+            if (expandedState) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text("X")
+                    Text("X")
+                    Text("X")
+                    Text("X")
+                }
+            }
+        }
+
+
     }
+}
+
+@Preview
+@Composable
+fun CustomItemPreview() {
+    CustomItem(practiceCard = PracticeCard("가다", "으려고", "가려고 했어요."))
 }
---
+++ app/src/main/java/com/example/koreancompose/ExpandableCard.kt
@@ -0,0 +1,93 @@
+package com.example.koreancompose
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExpandableCard() {
+    var expandedState by remember { mutableStateOf(false) }
+    val rotateState by animateFloatAsState(targetValue = if(expandedState) 180f else 0f)
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+            ),
+        shape = RoundedCornerShape(6.dp),
+        onClick = {
+            expandedState = !expandedState
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    modifier = Modifier
+                        .weight(6f),
+                    text = "My title",
+                    fontSize = MaterialTheme.typography.h6.fontSize,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                IconButton(
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium)
+                        .weight(1f)
+                        .rotate(rotateState),
+                    onClick = {
+                        expandedState = !expandedState
+                    }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Drop"
+                    )
+                }
+
+            }
+            if(expandedState) {
+                Text(text = "hello")
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun ExpandableFunctionPreview() {
+    ExpandableCard()
+}
